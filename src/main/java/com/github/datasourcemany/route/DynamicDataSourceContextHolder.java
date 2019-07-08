0d49367dd22ea310e34d1b914ee8bf6110729702
杨振华 <yangzhenhua@bizvane.cn>
2019-07-08 13:39:53 +0800

    修改

--- src/main/java/com/github/datasourcemany/route/DynamicDataSourceContextHolder.java
+++ src/main/java/com/github/datasourcemany/route/DynamicDataSourceContextHolder.java
@@ -8,11 +8,11 @@ public class DynamicDataSourceContextHolder {
 
     private static final ThreadLocal<String> contextHolder = new ThreadLocal<String>() {
         /**
-         * 将 master 数据源的 key作为默认数据源的 key
+         * 将 db1 数据源的 key作为默认数据源的 key
          */
         @Override
         protected String initialValue() {
-            return "master";
+            return "db1";
         }
     };
 
